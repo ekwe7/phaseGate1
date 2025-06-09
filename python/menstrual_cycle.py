fec31aa747e8bc51da170f9979200051fbe04ad2
Ekwe Emmanuel <ekwea753@gmail.com>
2025-06-10 00:42:35 +0100

    Update menstrual_cycle.py

--- python/menstrual_cycle.py
+++ python/menstrual_cycle.py
@@ -8,11 +8,12 @@ number_days_of_cycle = int(input("Enter number of days of your cycle: "))
 number_days_of_cycles = int(input("How many future cycles do you want to see? "))
 
 #MINIMUM CYCLE LENGTH BASED INFOR
-minimum_cycle_length = max(number_days_of_cycle, 25)
+minimum_cycle_length = max(number_days_of_cycle, 26)
 
 start_date = datetime.strptime(my_last_period, "%Y-%m-%d")
 
-print("\nMY NEXT CYCLES ARE: ")
+print("===MY NEXT CYCLES ARE===")
+
 for index in range(1, number_days_of_cycles + 1):
     next_date = start_date + timedelta(days=minimum_cycle_length * index)
     print(f"Cycle {index}: {next_date.strftime('%Y-%m-%d')}")
